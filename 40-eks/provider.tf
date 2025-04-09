5ced2f16048052db1e9fea00f115a1bf58cd9308
harshavardhan <kadiamharsha27@gmail.com>
2025-04-09 20:10:50 +0530

    Expense-Jenkins-infra-dev

---
+++ 40-eks/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.90.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "harsha81-remote-state-dev"
+    key            = "expense-eks"
+    region         = "us-east-1"
+    dynamodb_table = "81s-locking-dev"
+  }
+}
+
+provider "aws" {
+  # configuration options
+  region = "us-east-1"
+}
